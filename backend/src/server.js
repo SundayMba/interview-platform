118bc80d4bc7b4e0371e988d4d92905cac099da1
SundayMba <sundinoh@gmail.com>
2026-01-13 21:54:44 +0100

    chore: setup deployment features

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,8 @@
 import express from 'express';
 import ENV from './lib/env.js';
+import path from 'path';
+
+const __dirname = path.resolve(); // Get the current directory name
 
 const app = express(); // Create an Express applicatio server instance
 
@@ -7,7 +10,13 @@ app.get('/health', (req, res) => {
   res.status(200).json({ msg: 'Server is running successfully' });
 });
 
-console.log('Environment Variables:', ENV); // Log the loaded environment variables for verification
+if (ENV.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '../frontend/dist'))); // Serve static files from the frontend build directory
+
+  app.get('/*any', (req, res) => {
+    res.sendFile(path.join(__dirname, '../frontend', 'dist', 'index.html')); // Serve the main HTML file for any other routes
+  });
+}
 app.listen(ENV.PORT, () => {
   console.log(`Server is running on port ${ENV.PORT}`);
 }); // Start the server and listen on the specified port
