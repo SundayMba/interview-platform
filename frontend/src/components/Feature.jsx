2c81c4a05fd6cfea93ea600aed6f54d267232ad4
SundayMba <sundinoh@gmail.com>
2026-01-24 17:51:48 +0100

    chore: update homepage

---
+++ frontend/src/components/Feature.jsx
@@ -0,0 +1,59 @@
+import { Code2Icon, Users2Icon, UsersIcon, VideoIcon, ZapIcon } from "lucide-react";
+
+const Feature = () => {
+  return (
+    <div className="py-20 px-4">
+      
+      <div className="text-center mb-16">
+        <h2 className="text-4xl font-black mb-4">
+          Everything you need to  <span className="bg-linear-to-r from-primary via-secondary to-accent bg-clip-text text-transparent font-mono">Succeed</span>
+        </h2>
+        <p className="text-lg max-w-2xl mx-auto text-base-content/70">Powerful features designed to make your coding interviews seamless and productive</p>
+      </div>
+
+        {/* FEATURES GRID */}
+        <div className="grid md:grid-cols-3 gap-8">
+          {/* Feature 1 */}
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body items-center text-center">
+              <div className="size-16 bg-primary/10 rounded-2xl flex items-center justify-center mb-4">
+                <VideoIcon className="size-8 text-primary" />
+              </div>
+              <h3 className="card-title">HD Video Call</h3>
+              <p className="text-base-content/70">
+                Crystal clear video and audio for seamless communication during interviews
+              </p>
+            </div>
+          </div>
+
+          {/* Feature 2 */}
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body items-center text-center">
+              <div className="size-16 bg-primary/10 rounded-2xl flex items-center justify-center mb-4">
+                <Code2Icon className="size-8 text-primary" />
+              </div>
+              <h3 className="card-title">Live Code Editor</h3>
+              <p className="text-base-content/70">
+                Collaborate in real-time with syntax highlighting and multiple language support
+              </p>
+            </div>
+          </div>
+
+          {/* Feature 3 */}
+          <div className="card bg-base-100 shadow-xl">
+            <div className="card-body items-center text-center">
+              <div className="size-16 bg-primary/10 rounded-2xl flex items-center justify-center mb-4">
+                <UsersIcon className="size-8 text-primary" />
+              </div>
+              <h3 className="card-title">Easy Collaboration</h3>
+              <p className="text-base-content/70">
+                Share your screen, discuss solutions, and learn from each other in real-time
+              </p>
+            </div>
+          </div>
+      </div>
+    </div>
+  )
+}
+
+export default Feature;
---
+++ frontend/src/components/HeroSection.jsx
@@ -0,0 +1,82 @@
+import { SignInButton } from "@clerk/clerk-react";
+import { ArrowRightIcon, CheckIcon, VideoIcon, ZapIcon } from "lucide-react";
+
+const HeroSection = () => {
+  return (
+    <div className="grid lg:grid-cols-2 gap-12 items-center py-20 px-4">
+      {/* LEFT SECTION */}
+     <div className="space-y-8">
+      <div className="badge badge-lg badge-primary">
+        <ZapIcon />
+        <span>Real-Time Collaboration</span>
+      </div>
+
+      <h1 className="text-5xl lg:text-7xl font-black leading-tight">
+        <span className="bg-linear-to-r from-primary via-secondary to-accent bg-clip-text text-transparent">Code Together,</span> <br />
+        <span className="text-base-content">Learn Together</span>
+      </h1>
+      <p className="text-xl text-base-content/70 leading-relaxed max-w-xl">
+        The ultimate platform for collaborative coding interviews and pair programming. Connect face-to-face, code in real-time, and ace your technical interviews.
+      </p>
+
+      {/* FEATURE PILLS */}
+      <div className="flex flex-wrap gap-3">
+        <div className="badge badge-lg badge-outline">
+          <CheckIcon className="size-4 text-success" />
+          <span>Live Video Chat</span>
+        </div>
+        <div className="badge badge-lg badge-outline">
+          <CheckIcon className="size-4 text-success" />
+          <span>Code Editor</span>
+        </div>
+        <div className="badge badge-lg badge-outline">
+          <CheckIcon className="size-4 text-success" />
+          <span>Multi-Language</span>
+        </div>
+      </div>
+
+      {/* CTA */}
+      <div className="flex flex-wrap gap-4">
+        <SignInButton mode="modal">
+          <button className="btn btn-primary btn-lg">
+            <span>Start Coding Now</span>
+            <ArrowRightIcon className="size-5" />
+          </button>
+        </SignInButton>
+
+        <button className="btn btn-outline btn-lg">
+          <VideoIcon className="size-5"/>
+          <span>Learn More</span>
+        </button>
+      </div>
+
+      {/* STATS */}
+      <div className="stats stats-vertical lg:stats-horizontal shadow-lg bg-base-100">
+        <div className="stat">
+          <div className="stat-value text-primary">10K+</div>
+          <div className="stat-title">Active Users</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-value text-secondary">50k+</div>
+          <div className="stat-title">Sessions</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-value text-accent">99.9%</div>
+          <div className="stat-title">Uptime</div>
+        </div>
+      </div>
+     </div>
+
+      {/* RIGHT SECTION - IMAGE */}
+        <img
+          src="/hero.png"
+          alt="Code Collaboration Platform"
+          className="w-full h-auto object-cover rounded-3xl border-4 border-base-100 shadow-2xl hover:scale-105 transition-all duration-500"
+        />
+    </div>
+  )
+}
+
+export default HeroSection;
