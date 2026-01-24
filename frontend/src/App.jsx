192e3dfcf2cd20b5d49a0d8ed9b816c5e00c792c
SundayMba <sundinoh@gmail.com>
2026-01-25 00:58:55 +0100

    chore: add dashboard

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,23 @@
 import HomePage from './pages/HomePage';
 import { Routes, Route } from 'react-router';
 import { Toaster } from 'react-hot-toast';
+import { useAuth } from '@clerk/clerk-react'
+import DashBoard from './pages/Dashboard';
+
 
 function App() {
+  const { isSignedIn, isLoaded } = useAuth();
+
+  //wait until auth completes initialiazation
+  if (!isLoaded) {
+    return null;
+  }
+  
   return (
     <>
       <Routes>
-        <Route path="/" element={<HomePage />} />
+        <Route path="/" element={ !isSignedIn ? <HomePage /> : <DashBoard /> } />
+        <Route path="/dashboard" element={ isSignedIn ? <DashBoard /> : <HomePage /> } />
       </Routes>
       <Toaster />
     </>
